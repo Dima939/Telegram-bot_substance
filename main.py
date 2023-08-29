9f455a7868f03630f8576af7f3e5d6fd9da86059
Dima939 <47751771+Dima939@users.noreply.github.com>
2023-08-29 15:58:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import telebot
+
+bot = telebot.TeleBot('5902384286:AAEyVy_SyGVCIX3Q0EJyChPUhEL1MecldoU')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Welcome!!!')
+
+
+@bot.message_handler(content_types=['text'])
+def repeat_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
 
